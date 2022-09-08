6e642ba161a9b22445a2b870bc24df758eb4e62f
jm0601 <jimin600155@naver.com>
2022-09-08 11:35:47 +0900

    확인 끝.

--- src/com/kh/JdbcMain.java
+++ src/com/kh/JdbcMain.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+// git . . .
 public class JdbcMain {
     public static void main(String[] args) {
         Connection conn = null;
